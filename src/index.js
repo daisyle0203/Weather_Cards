6b3f4f511e38a1d447052a018b02495c62c4d537
Daisy Le <daisy.le0203@gmail.com>
2022-09-24 17:03:21 -0500

    added Loading image

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "./style/App.css"
 import React from "react"
 import ReactDOM from "react-dom"
 import SeasonDisplay from "./SeasonDisplay"
@@ -17,7 +18,7 @@ class App extends React.Component {
   }
 
   // render method that App borrows from react component
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>
     }
@@ -26,7 +27,11 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />
     }
 
-    return <Spinner />
+    return <Spinner message="Please accept location request" />
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>
   }
 }
 
